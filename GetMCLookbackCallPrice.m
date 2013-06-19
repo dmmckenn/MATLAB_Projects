c9acb58f7b8997da4dc21d83432da22d3c52745d
dmmckenn <davidmckenna10@gmail.com>
2013-06-19 19:20:14 -0400

    Update GetMCLookbackCallPrice.m

--- GetMCLookbackCallPrice.m
+++ GetMCLookbackCallPrice.m
@@ -1,6 +1,6 @@
 function call_price = GetMCLookbackCallPrice( stock_init, strike, num_periods, volatility, exp_time, int_rate, num_iterations)
-% Get discounted expected payoff of a lookback call option has the payoff
-% of the stock's maximum price over stock_init to exp_time minus the terminal
+% Get discounted expected payoff of a lookback call option whose payoff
+% is the stock's maximum price over stock_init to exp_time minus the terminal
 % price of the stock. Uses Hull binomial tree parameters
 % over num_periods. Performs num_iterations paths in Monte-Carlo
 % simulation. Volatility of stock is voltatility, risk-free rate over
